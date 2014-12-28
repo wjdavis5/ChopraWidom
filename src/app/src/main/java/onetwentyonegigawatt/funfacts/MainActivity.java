a16b5743e63593009fdccc75a6dc8dc7367beb23
William Davis <william.j.davis5@gmail.com>
2014-12-28 17:53:58 -0500

    Started working on the facebook share - realized we cannot force text using the ShareDialogBuilder, have to rework this using the graph api

--- src/app/src/main/java/onetwentyonegigawatt/funfacts/MainActivity.java
+++ src/app/src/main/java/onetwentyonegigawatt/funfacts/MainActivity.java
@@ -2,24 +2,47 @@ package onetwentyonegigawatt.funfacts;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.content.ClipboardManager;
+import com.facebook.AppEventsLogger;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
 
 import java.lang.reflect.Type;
 
 
 public class MainActivity extends Activity {
+
+    private UiLifecycleHelper uiHelper;
+    private Activity thisAcitivty = this;
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +53,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         final TypeWriter factLabel = (TypeWriter) findViewById(R.id.txtFact);
+        final ImageButton btnFbook = (ImageButton) findViewById(R.id.btnShareFbook);
         Button showFactButton = (Button) findViewById(R.id.btnShowFact);
         final DownloadWebpageTask dWt = new DownloadWebpageTask(MainActivity.this);
         View.OnClickListener showFactButtonOnClickListener = new View.OnClickListener() {
@@ -41,7 +65,7 @@ public class MainActivity extends Activity {
                             getSystemService(Context.CONNECTIVITY_SERVICE);
                     NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                     if (networkInfo != null && networkInfo.isConnected()) {
-                        dWt.execute("");
+                        new DownloadWebpageTask(MainActivity.this).execute("");
                     } else {
                         factLabel.animateText("No network connection available.");
                     }
@@ -53,7 +77,78 @@ public class MainActivity extends Activity {
         };
         showFactButton.setOnClickListener(showFactButtonOnClickListener);
 
+        View.OnClickListener showFbookDialog = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Wisdom",
+                        factLabel.getText().toString() + " #wisdomofchopra #choprawisdom");
+                clipboard.setPrimaryClip(clip);
+                FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(thisAcitivty)
+                        .setLink(null)
+                        .setCaption(factLabel.getText().toString() + " #wisdomofchopra #choprawisdom")
+                        .setDescription(factLabel.getText().toString() + " #wisdomofchopra #choprawisdom")
+                        .build();
+                uiHelper.trackPendingDialogCall(shareDialog.present());
+                clipboard.getPrimaryClip();
+            }
+        };
+        btnFbook.setOnClickListener(showFbookDialog);
+
+        uiHelper = new UiLifecycleHelper(this, null){
+            @Override
+            public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                
+            }
+
+            @Override
+            public void onResume() {
+                super.onResume();
+                uiHelper.onResume();
+            }
+
+            @Override
+            public void onSaveInstanceState(Bundle outState) {
+                super.onSaveInstanceState(outState);
+                uiHelper.onSaveInstanceState(outState);
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                uiHelper.onPause();
+            }
+
+            @Override
+            public void onDestroy() {
+                super.onDestroy();
+                uiHelper.onDestroy();
+            }
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                super.onActivityResult(requestCode, resultCode, data);
+
+                uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+                    @Override
+                    public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+                        Log.e("Activity", String.format("Error: %s", error.toString()));
+                    }
+
+                    @Override
+                    public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+                        Log.i("Activity", "Success!");
+                    }
+                });
+            }
+
+
+        };
+        uiHelper.onCreate(savedInstanceState);
+
     }
+
 
     private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
         private ProgressDialog mDialog;
@@ -78,9 +173,8 @@ public class MainActivity extends Activity {
             try {
                 TypeWriter factLabel = (TypeWriter) findViewById(R.id.txtFact);
                 factLabel.animateText(result);
-                ((Button) findViewById(R.id.btnShowFact)).setEnabled(true);
                 mDialog.hide();
-
+                ((Button) findViewById(R.id.btnShowFact)).setEnabled(true);
             }
             catch(Exception e){
                 e.printStackTrace();
